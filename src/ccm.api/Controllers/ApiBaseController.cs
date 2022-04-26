bb925f35063207ee965e59d0a87778e99f04070b
ecabigting <ericcabigting@outlook.com>
2022-04-26 20:16:29 +0400

    started the base api controller and student controller

--- src/ccm.api/Controllers/ApiBaseController.cs
+++ src/ccm.api/Controllers/ApiBaseController.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ccm.api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ApiBaseController
+    public class ApiBaseController
     {
         
     }
